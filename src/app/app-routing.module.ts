3b5d011bb35cb8e297ceec0a2ae4cef24667e90c
Adriano Flechilla <adrianof.dev@gmail.com>
2023-09-02 11:10:25 -0400

    Initial commit
    Contains the files from the template
    - These files are contained in the folder: template-bundle
    - The idea is to take the needed elements from these files and remove them
    on demand

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,99 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './template-bundle/layouts/blank/blank.component';
+import { FullComponent } from './template-bundle/layouts/full/full.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/dashboards/dashboard1',
+        pathMatch: 'full',
+      },
+      {
+        path: 'starter',
+        loadChildren: () =>
+          import('./template-bundle/pages/pages.module').then((m) => m.PagesModule),
+      },
+      {
+        path: 'dashboards',
+        loadChildren: () =>
+          import('./template-bundle/pages/dashboards/dashboards.module').then(
+            (m) => m.DashboardsModule
+          ),
+      },
+      {
+        path: 'ui-components',
+        loadChildren: () =>
+          import('./template-bundle/pages/ui-components/ui-components.module').then(
+            (m) => m.UicomponentsModule
+          ),
+      },
+      {
+        path: 'forms',
+        loadChildren: () =>
+          import('./template-bundle/pages/forms/forms.module').then((m) => m.FormModule),
+      },
+      {
+        path: 'charts',
+        loadChildren: () =>
+          import('./template-bundle/pages/charts/charts.module').then((m) => m.ChartsModule),
+      },
+      {
+        path: 'apps',
+        loadChildren: () =>
+          import('./template-bundle/pages/apps/apps.module').then((m) => m.AppsModule),
+      },
+      {
+        path: 'widgets',
+        loadChildren: () =>
+          import('./template-bundle/pages/widgets/widgets.module').then((m) => m.WidgetsModule),
+      },
+      {
+        path: 'tables',
+        loadChildren: () =>
+          import('./template-bundle/pages/tables/tables.module').then((m) => m.TablesModule),
+      },
+      {
+        path: 'theme-pages',
+        loadChildren: () =>
+          import('./template-bundle/pages/theme-pages/theme-pages.module').then(
+            (m) => m.ThemePagesModule
+          ),
+      },
+    ],
+  },
+  {
+    path: '',
+    component: BlankComponent,
+    children: [
+      {
+        path: 'authentication',
+        loadChildren: () =>
+          import('./template-bundle/pages/authentication/authentication.module').then(
+            (m) => m.AuthenticationModule
+          ),
+      },
+      {
+        path: 'landingpage',
+        loadChildren: () =>
+          import('./template-bundle/pages/theme-pages/landingpage/landingpage.module').then(
+            (m) => m.LandingPageModule
+          ),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'authentication/error',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
