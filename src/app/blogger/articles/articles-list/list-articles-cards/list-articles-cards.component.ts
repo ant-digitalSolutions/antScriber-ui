f3af8dba59a23832b89af25e608b4aeb038adc9d
Adriano Flechilla <adrianof.dev@gmail.com>
2023-09-13 21:45:07 -0400

    Open the card to read the article's body

--- src/app/blogger/articles/articles-list/list-articles-cards/list-articles-cards.component.ts
+++ src/app/blogger/articles/articles-list/list-articles-cards/list-articles-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IArticleDetailsDto } from 'src/app/blogger/dto/article-details.dto';
 import { ArticleService } from 'src/app/blogger/services/article.service';
 import { environment } from 'src/environments/environment';
@@ -8,23 +8,58 @@ import { environment } from 'src/environments/environment';
   templateUrl: './list-articles-cards.component.html',
   styleUrls: ['./list-articles-cards.component.scss']
 })
-export class ListArticlesCardsComponent {
-editArticle(_t4: IArticleDetailsDto) {
-throw new Error('Method not implemented.');
-}
+export class ListArticlesCardsComponent implements OnChanges {
+
 
   @Input()
   articlesToRender: IArticleDetailsDto[] = [];
+
+  articleSelected: { articleId: number, isSelected: boolean }[];
 
   constructor(private articleService: ArticleService) {
 
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['articlesToRender']) {
+      this.articleSelected = [];
+      for (let i = 0; i < this.articlesToRender.length; i++) {
+        this.articleSelected.push({
+          articleId: this.articlesToRender[i].id,
+          isSelected: false
+        })
+
+      }
+    }
+  }
+
   selectArticle(article: IArticleDetailsDto) {
+    this.articleSelected.forEach(a => {
+      if (a.articleId === article.id) {
+        a.isSelected = true;
+      } else {
+        a.isSelected = false;
+      }
+    });
+
+    // const elements = document.getElementById(`article-card-${article.id}`);
+    // if (elements) {
+    //   setTimeout(() => {
+    //     elements.scrollIntoView();
+    //   }, 200)
+    // }
+
+
+  }
+
+  editArticle(article: IArticleDetailsDto) {
     this.articleService.navigateToGenerateFullArticle(article)
+
   }
 
   imageFullPath(article: IArticleDetailsDto): string {
     return environment.apiUrl + article.featureImagePath;
   }
+
+
 }
