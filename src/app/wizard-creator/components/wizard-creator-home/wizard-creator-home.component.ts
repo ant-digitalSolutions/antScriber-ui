218a327132101afd387f1a387ac6d8a153e0e6e4
Adriano Flechilla <adrianof.dev@gmail.com>
2023-10-03 11:46:19 -0400

    SCroll to the bottom of the view after repsonse from wizard creator
    After fixing/improving the document area, we should move this implementation to
    the corresponding component

--- src/app/wizard-creator/components/wizard-creator-home/wizard-creator-home.component.ts
+++ src/app/wizard-creator/components/wizard-creator-home/wizard-creator-home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { WizardCreatorService } from '../../services/wizard-creator.service';
 
 @Component({
   selector: 'app-wizard-creator-home',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./wizard-creator-home.component.scss']
 })
 export class WizardCreatorHomeComponent {
+  componentDestroyed$: Subject<boolean> = new Subject();
+
+  constructor(private _wizard: WizardCreatorService) {
+
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next(true)
+    this.componentDestroyed$.complete()
+  }
+
+  ngOnInit(): void {
+    this.setListeners();
+  }
+
+  setListeners() {
+    this._wizard.wizardCreatedContent$.pipe(takeUntil(this.componentDestroyed$))
+      .subscribe(() => {
+        this.scrollToBottom();
+      })
+  }
+
+  scrollToBottom(): void {
+    try {
+      const wizardContent = document.querySelector('.wizard-content');
+      wizardContent!.scrollTop = wizardContent!.scrollHeight;
+    } catch (err) {
+      console.error(`Error Scrolling the content: ${err}`)
+    }
+  }
 
 }
