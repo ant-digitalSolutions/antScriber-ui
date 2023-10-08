d8394789b67885b467dcc4bf541c7f4794946e5c
Adriano Flechilla <adrianof.dev@gmail.com>
2023-10-08 10:10:06 -0400

    Fix bug
    The data should always be a string. Is responsibility of the data owner to parse it
    if necessary

--- src/app/common/services/cache/local-storage-cache.service.ts
+++ src/app/common/services/cache/local-storage-cache.service.ts
@@ -5,10 +5,10 @@ import { ICacheService } from './cache-service.interface';
 export class LocalStorageCacheService implements ICacheService {
   getData(dataKey: string) {
     const data = localStorage.getItem(dataKey);
-    return data ? JSON.parse(data) : null;
+    return data;
   }
 
-  setData(key: string, data: any): boolean {
+  setData(key: string, data: string): boolean {
     try {
        localStorage.setItem(key, data);
        return true;
