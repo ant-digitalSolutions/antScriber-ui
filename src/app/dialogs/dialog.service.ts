860f1f98b0a8c3b8c654fa876a7e69f4ba1dd6a7
Adriano Flechilla <adrianof.dev@gmail.com>
2023-10-09 19:18:56 -0400

    Extend method by receiving the data object

--- src/app/dialogs/dialog.service.ts
+++ src/app/dialogs/dialog.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DialogWithSingleInputComponent } from './components/dialog-with-single-input/dialog-with-single-input.component';
 import { Subject } from 'rxjs';
+import { DialogCustomData_SingleInput } from './dto/dialog-data';
 
 @Injectable({
   providedIn: 'root'
@@ -36,6 +37,34 @@ export class DialogService {
         labelText,
         okBtnText
        },
+    });
+
+    return dialogRef;
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The dialog was closed');
+    //   this._dialogResult.next(result);
+    // });
+  }
+
+  /**
+   * Opens a dialog that contain a text input
+   * 
+   * THe text of the dialog is customizable for the user.
+   * 
+   * It return the dialogRef which contains the data and methods
+   * for the created dialog. To get the result: dialog.afterClosed().subscribe(result =>)
+   *
+   * @param {string} title
+   * @param {string} placeholder
+   * @param {string} labelText
+   * @param {string} okBtnText
+   * @return {*}  {MatDialogRef<any, any>}
+   * @memberof DialogService
+   */
+  openDialogWithSingleInput_v2(dialogData: DialogCustomData_SingleInput): MatDialogRef<any, any> {
+    const dialogRef = this.dialog.open(DialogWithSingleInputComponent, {
+      data: dialogData,
     });
 
     return dialogRef;
