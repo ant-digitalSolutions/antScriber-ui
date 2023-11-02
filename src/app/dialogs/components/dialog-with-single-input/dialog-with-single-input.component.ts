e14019d5ce60cb8f1d942154bf82450fd751f8f2
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-02 17:30:16 -0400

    Check the value of the input, has to be required

--- src/app/dialogs/components/dialog-with-single-input/dialog-with-single-input.component.ts
+++ src/app/dialogs/components/dialog-with-single-input/dialog-with-single-input.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Validators } from 'ngx-editor';
 import { DialogCustomData_SingleInput } from '../../dto/dialog-data';
 
 @Component({
@@ -7,12 +9,24 @@ import { DialogCustomData_SingleInput } from '../../dto/dialog-data';
   templateUrl: './dialog-with-single-input.component.html',
   styleUrls: ['./dialog-with-single-input.component.scss']
 })
-export class DialogWithSingleInputComponent {
+export class DialogWithSingleInputComponent implements OnInit {
+
+  form: FormControl;
 
   constructor(
     public dialogRef: MatDialogRef<DialogWithSingleInputComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogCustomData_SingleInput,
   ) { }
+
+  ngOnInit(): void {
+    this.form = new FormControl(null, [Validators.required(), Validators.maxLength(100)])
+  }
+
+  ok() {
+    if (this.form.valid) {
+      this.dialogRef.close(this.form.value);
+    }
+  }
 
   onClose(): void {
     this.dialogRef.close();
