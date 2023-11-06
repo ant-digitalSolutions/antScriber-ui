b07faf57fee628c4f9f58a240cfc41f297da78fc
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-06 17:42:21 -0500

    Implement basic style and logic on the sidebar

--- src/app/user-settings/components/user-settings-navbar/user-settings-navbar.component.ts
+++ src/app/user-settings/components/user-settings-navbar/user-settings-navbar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoreService } from 'src/app/services/core.service';
 
 @Component({
   selector: 'app-user-settings-navbar',
   templateUrl: './user-settings-navbar.component.html',
   styleUrls: ['./user-settings-navbar.component.scss']
 })
-export class UserSettingsNavbarComponent {
+export class UserSettingsNavbarComponent implements OnInit {
+
+  options = this.settings.getOptions();
+
+  constructor(private settings: CoreService,) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  toggleSidebar() {
+    this.options.sidenavOpened = !this.options.sidenavOpened;
+  }
+
+
+  public get isMobileScreen(): boolean {
+    return window.innerWidth < 960;
+  }
+
 
 }
