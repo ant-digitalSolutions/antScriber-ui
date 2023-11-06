1f76c78ba3b3bc396891bdba6a2eac2142844d4d
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-06 14:15:55 -0500

    Generate base files for the user settings area

---
+++ src/app/user-settings/user-settings-home/user-settings-home.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserSettingsHomeComponent } from './user-settings-home.component';
+
+describe('UserSettingsHomeComponent', () => {
+  let component: UserSettingsHomeComponent;
+  let fixture: ComponentFixture<UserSettingsHomeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [UserSettingsHomeComponent]
+    });
+    fixture = TestBed.createComponent(UserSettingsHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
