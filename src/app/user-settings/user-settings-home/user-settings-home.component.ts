0759cbf61a61d682c8f2431316be2d5795f9c3df
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-06 14:15:55 -0500

    Generate base files for the user settings area

---
+++ src/app/user-settings/user-settings-home/user-settings-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-settings-home',
+  templateUrl: './user-settings-home.component.html',
+  styleUrls: ['./user-settings-home.component.scss']
+})
+export class UserSettingsHomeComponent {
+
+}
---
+++ src/app/user-settings/user-settings.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserSettingsHomeComponent } from './user-settings-home/user-settings-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserSettingsHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserSettingsModule { }
