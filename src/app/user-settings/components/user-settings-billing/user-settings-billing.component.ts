7d52bb2eb244b38481dded4f602dfbba4b969d9e
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-06 21:55:05 -0500

    Generate files for the billing area

---
+++ src/app/user-settings/components/user-settings-billing/user-settings-billing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-settings-billing',
+  templateUrl: './user-settings-billing.component.html',
+  styleUrls: ['./user-settings-billing.component.scss']
+})
+export class UserSettingsBillingComponent {
+
+}
