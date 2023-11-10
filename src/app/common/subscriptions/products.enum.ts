7da00cc5cc684acec094a9c37814c5b560c23160
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-09 22:35:10 -0500

    Implement logic to check if the user is premium

---
+++ src/app/common/subscriptions/products.enum.ts
@@ -0,0 +1,14 @@
+/**
+ * Indicates the type of subscription that we offer.
+ *
+ * The values of the enum represent the ProductId on Stripe.
+ *
+ * @export
+ * @enum {number}
+ */
+export enum ProductsEnum {
+  Solo = 'prod_OyZtqjmoVGB2Q7',
+  Flow = 'prod_OyZuhZGvmn8L0D',
+  Horizon = 'prod_not_created',
+  FREE = 'free',
+}
