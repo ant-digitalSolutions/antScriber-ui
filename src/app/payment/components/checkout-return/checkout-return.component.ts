c28d4de06f7abada458172fd81443d347c94f50b
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-11 14:55:55 -0500

    IMplement logic ro render needed data after checkout

--- src/app/payment/components/checkout-return/checkout-return.component.ts
+++ src/app/payment/components/checkout-return/checkout-return.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/user/services/user.service';
+import { InvoiceDto } from '../../dtos/invoice.dto';
+import { PaymentSessionDto } from '../../dtos/payment-session.dto';
+import { SubscriptionResponseDTO } from '../../dtos/subscription-response.dto';
 import { PaymentService } from '../../services/payment.service';
+import { PlanDto } from './../../dtos/plan.dto';
 @Component({
   selector: 'app-checkout-return',
   templateUrl: './checkout-return.component.html',
@@ -17,6 +21,8 @@ export class CheckoutReturnComponent implements OnInit {
 
   userFirstName: string;
 
+  paymentSession: PaymentSessionDto;                                 
+
   constructor(private route: ActivatedRoute, private _paymentService: PaymentService, private _userService: UserService) {}
 
   ngOnInit() {
@@ -31,9 +37,30 @@ export class CheckoutReturnComponent implements OnInit {
     this._paymentService.getStripeSession(this.sessionId).subscribe(r => {
       if(r.success) {
         this.paymentSucceed = r.data.paymentStatus === 'paid';
+        this.paymentSession  = r.data;
       } else {
         this.paymentSucceed = false;
       }
+
+      this.dataReady = true;
     })
   }
+
+  
+  public get invoice() : InvoiceDto {
+    return this.paymentSession.invoice as InvoiceDto;
+  }
+
+  
+  public get subscription() : SubscriptionResponseDTO {
+    return this.paymentSession.subscription as SubscriptionResponseDTO;
+  }
+
+  
+  public get plan() : PlanDto {
+    return this.subscription.plan!;
+  }
+  
+  
+  
 }
