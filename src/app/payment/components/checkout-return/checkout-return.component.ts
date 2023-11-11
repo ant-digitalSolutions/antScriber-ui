3eeab3e6e8216f92e92bd5a5dda3c48492db6c84
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-11 09:57:28 -0500

    Implement logic to get the latest checkout session
    - AFter a checkout session, redirect the user to the CheckoutReturn component
    - It get the sessionID from queryParam (comes from Stripe) and then get the
    session object from the server
    - Render elements based on the session success or not

--- src/app/payment/components/checkout-return/checkout-return.component.ts
+++ src/app/payment/components/checkout-return/checkout-return.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/user/services/user.service';
+import { PaymentService } from '../../services/payment.service';
 @Component({
   selector: 'app-checkout-return',
   templateUrl: './checkout-return.component.html',
-  styleUrls: ['./checkout-return.component.scss']
+  styleUrls: ['./checkout-return.component.scss'],
 })
-export class CheckoutReturnComponent {
+export class CheckoutReturnComponent implements OnInit {
 
+  sessionId: string;
+
+  paymentSucceed: boolean;
+
+  dataReady = false;
+
+  userFirstName: string;
+
+  constructor(private route: ActivatedRoute, private _paymentService: PaymentService, private _userService: UserService) {}
+
+  ngOnInit() {
+    this.getSessionParam();
+
+    this.userFirstName = this._userService.userFirstName!;
+  }
+
+  getSessionParam() {
+    this.sessionId = this.route.snapshot.queryParams['session_id'];
+
+    this._paymentService.getStripeSession(this.sessionId).subscribe(r => {
+      if(r.success) {
+        this.paymentSucceed = r.data.paymentStatus === 'paid';
+      } else {
+        this.paymentSucceed = false;
+      }
+    })
+  }
 }
