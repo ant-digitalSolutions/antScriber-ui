4a37f0f370e5ae8d43b2f9a9a9d7a0bae4678085
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-14 14:14:53 -0500

    Improve UX in the subscription details modal

--- src/app/pricing/pricing.module.ts
+++ src/app/pricing/pricing.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import * as TablerIcons from 'angular-tabler-icons/icons';
+import { AnalyticsModule } from '../analytics/analytics.module';
 import { MaterialModule } from '../material.module';
 import { AppPricingComponent } from './components/pricing/pricing.component';
 
@@ -20,6 +21,7 @@ import { AppPricingComponent } from './components/pricing/pricing.component';
     ReactiveFormsModule,
     TablerIconsModule.pick(TablerIcons),
     MatNativeDateModule,
+    AnalyticsModule 
   ],
   exports: [AppPricingComponent]
 })
