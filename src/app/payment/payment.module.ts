071de401eb698282111cb05000e5536d865d1c7d
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-14 13:01:50 -0500

    Render the real data for the words usage details

--- src/app/payment/payment.module.ts
+++ src/app/payment/payment.module.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AnalyticsModule } from '../analytics/analytics.module';
 import { MaterialModule } from '../material.module';
 import { CheckoutReturnComponent } from './components/checkout-return/checkout-return.component';
+import { ListPaymentsComponent } from './components/list-payments/list-payments.component';
 import { SubscriptionCheckoutComponent } from './components/subscription-checkout/subscription-checkout.component';
+import { SubscriptionDetailsComponent } from './components/subscription-details/subscription-details.component';
 import { PaymentsModuleRoutes } from './payments.routing';
 import { PaymentService } from './services/payment.service';
-import { ListPaymentsComponent } from './components/list-payments/list-payments.component';
-import { SubscriptionDetailsComponent } from './components/subscription-details/subscription-details.component';
 
 @NgModule({
   declarations: [SubscriptionCheckoutComponent, CheckoutReturnComponent, ListPaymentsComponent, SubscriptionDetailsComponent],
-  imports: [CommonModule, RouterModule.forChild(PaymentsModuleRoutes), MaterialModule],
+  imports: [CommonModule, RouterModule.forChild(PaymentsModuleRoutes), MaterialModule, AnalyticsModule],
   providers: [PaymentService],
 })
 export class PaymentModule {}
