9b9b82a7376d1daebd4220a82a93364596a2f104
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-16 11:34:48 -0500

    Logic to select assistant and threads
    - List the available assistants
    - Render the assistant in the left nav
    - If the user select an assistant, update the route params
    - If the user select a thread, update the route params with the
    assistantId and threadId
    - List the thread messages after the user select a thread

--- src/app/chat-assistant/components/chat-assistant-home/chat-assistant-home.component.ts
+++ src/app/chat-assistant/components/chat-assistant-home/chat-assistant-home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { ChatAssistantsService } from '../../services/chat-assistants.service';
+import { ChatThreadsService } from '../../services/chat-threads.service';
 
 // chat-assistant-home.component.ts
 interface ChatMessage {
@@ -10,21 +13,56 @@ interface ChatMessage {
 @Component({
   selector: 'app-chat-assistant-home',
   templateUrl: './chat-assistant-home.component.html',
-  styleUrls: ['./chat-assistant-home.component.scss']
+  styleUrls: ['./chat-assistant-home.component.scss'],
 })
 export class ChatAssistantHomeComponent implements OnInit {
-
   previousChats: string[] = ['Chat 1', 'Chat 2', 'Chat 3'];
   chatHistory: ChatMessage[] = [
     { sender: 'user', content: 'Hello, how are you?' },
-    { sender: 'assistant', content: 'I am good, thank you! How can I assist you today?' }
+    {
+      sender: 'assistant',
+      content: 'I am good, thank you! How can I assist you today?',
+    },
   ];
   newMessage: string = '';
 
-  constructor() { }
+  componentDestroyed$: Subject<boolean> = new Subject();
+
+  isLoading = false;
+
+  private _assistantId?: string;
+  private _threadId?: string;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _chatThreadService: ChatThreadsService,
+    private _chatAssistantService: ChatAssistantsService
+  ) {}
 
   ngOnInit(): void {
-    // Normally, load previous chats and chat history from a service or local storage
+    this.setListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next(true);
+    this.componentDestroyed$.complete();
+  }
+
+  setListeners() {
+    this._route.params
+      .pipe(takeUntil(this.componentDestroyed$))
+      .subscribe((params) => {
+        if (params) {
+          if (params['assistantId']) {
+            this._assistantId = params['assistantId'];
+            this._chatAssistantService._currentAssistant = this._assistantId!;
+          }
+          if (params['threadId']) {
+            this._threadId = params['threadId'];
+            this._chatThreadService.listThreadMessages(this._threadId!).subscribe();
+          }
+        }
+      });
   }
 
   sendMessage(): void {
@@ -43,5 +81,4 @@ export class ChatAssistantHomeComponent implements OnInit {
       this.chatHistory.push({ sender: 'assistant', content: mockResponse });
     }, 1000); // Simulate response delay
   }
-
 }
