fce8962304f93802d705ce7b85c80296b6ce363f
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-15 23:36:36 -0500

    Implement logic to list the Threads history

--- src/app/chat-assistant/components/chat-list-sidebar/chat-list-sidebar.component.ts
+++ src/app/chat-assistant/components/chat-list-sidebar/chat-list-sidebar.component.ts
@@ -1,18 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatThreadDto } from '../../dtos/chat-thread.dto';
+import { ChatAssistantService } from '../../services/chat-assistant.service';
 
 @Component({
   selector: 'app-chat-list-sidebar',
   templateUrl: './chat-list-sidebar.component.html',
-  styleUrls: ['./chat-list-sidebar.component.scss']
+  styleUrls: ['./chat-list-sidebar.component.scss'],
 })
 export class ChatListSidebarComponent implements OnInit {
 
   previousChats: string[] = ['Chat 1', 'Chat 2', 'Chat 3'];
 
-  constructor() { }
+  _chatThreads: ChatThreadDto[] | undefined;
+
+  constructor(private _chatService: ChatAssistantService) {}
 
   ngOnInit(): void {
-    // Load previous chats from a service or local storage
+    this.listThreadHistory()
   }
 
+  listThreadHistory() {
+    this._chatService.listThreadHistory().subscribe((r) => {
+      if (r.success) {
+        this._chatThreads = r.data;
+      }
+    });
+  }
+
+  selectThread(chatThread: ChatThreadDto) {
+    this._chatService.selectThread(chatThread).subscribe(r => {
+      if (r.success) {
+        console.log(r.data)
+      }
+    })
+    }
+  
+  public get chatThreadsNames() : string[] | undefined {
+    return this._chatThreads?.map(t => t.threadName);
+  }
+  
 }
