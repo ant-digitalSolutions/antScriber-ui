6657ef0c3cfc0ab5e3d2573aa1ae74b08b830c1b
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-25 14:07:42 -0500

    Change confirmation modal to render plan and payment details

--- src/app/payment/components/subscription-update-confirmation/subscription-update-confirmation.component.ts
+++ src/app/payment/components/subscription-update-confirmation/subscription-update-confirmation.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { SubscriptionUpdateDTO } from '../../dtos/subscription-update.dto';
+import {
+  SubscriptionUpdateDTO,
+  SubscriptionUpdateType,
+} from '../../dtos/subscription-update.dto';
 import { PaymentService } from '../../services/payment.service';
 
 @Component({
@@ -20,11 +23,19 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
     public dialogRef: MatDialogRef<SubscriptionUpdateConfirmationComponent>,
     @Inject(MAT_DIALOG_DATA) public data: SubscriptionUpdateDTO,
     private _paymentService: PaymentService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
-    //
+    this.isUpgrade = this.data.status === SubscriptionUpdateType.Upgrade;
+
+    if (this.data.newPlanName || this.data.newPlanName == '') {
+      this.data.newPlanName = 'Flow';
+      this.data.newPlanCost = 45;
+      this.data.toPayNow = 34;
+      this.data.currency = 'USD';
+      this.data.moneyBalance = 240;
+    }
+
     if (this.isUpgrade) {
       // todo: check if is upgrade
       this.mainBtnText = 'Confirm';
@@ -37,12 +48,11 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
 
   onConfirm(): void {
     this.isLoading = true;
-    this._paymentService.updateSubscription(this.data)
-    .subscribe(r => {
+    this._paymentService.updateSubscription(this.data).subscribe((r) => {
       if (r.success) {
         console.log('subscription updated');
       }
-    })
+    });
   }
 
   onCancel(): void {
@@ -50,11 +60,36 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
   }
 
   public get dataSource() {
-      return [
-        { label: 'New Plan Name', value: 'Flow' },
-        { label: 'Plan Pricing', value: '302' },
-        // { label: 'Plan Pricing', value: `${(this.subscription!.plan!.amount / 100).toFixed(2)} ${this.subscription?.currency?.toUpperCase()}` },
-        // { label: 'Billed', value: this.subscription?.plan?.interval === 'year' ? 'Yearly' : 'Monthly' }
-      ];
-    }
+    return [
+      { label: 'New Plan Name', value: this.data.newPlanName },
+      {
+        label: 'Plan Pricing',
+        value: `$${this.data.newPlanCost} ${this.data.currency}`,
+      },
+      {
+        label: 'Billing Interval',
+        value: this.data.billed === 'yearly' ? 'Yearly' : 'Monthly',
+      },
+
+      // { label: 'Plan Pricing', value: `${(this.subscription!.plan!.amount / 100).toFixed(2)} ${this.subscription?.currency?.toUpperCase()}` },
+    ];
+  }
+
+  public get dataSourcePaymentDetails() {
+    return [
+      {
+        label: 'Your Balance',
+        value: `$${this.data.moneyBalance} ${this.data.currency}`,
+      },
+      {
+        label: 'To Pay Now',
+        value: `$${this.data.toPayNow} ${this.data.currency}`,
+      },
+      // { label: 'Plan Pricing', value: `${(this.subscription!.plan!.amount / 100).toFixed(2)} ${this.subscription?.currency?.toUpperCase()}` },
+    ];
+  }
+
+  public get hasPositiveBalance(): boolean {
+    return this.data.moneyBalance > 0;
+  }
 }
--- src/app/payment/dtos/subscription-update.dto.ts
+++ src/app/payment/dtos/subscription-update.dto.ts
@@ -1,4 +1,3 @@
-import { InvoiceDto } from './invoice.dto';
 
 export enum SubscriptionUpdateType {
   Upgrade = 'upgrade',
@@ -9,8 +8,37 @@ export enum SubscriptionUpdateType {
 
 export class SubscriptionUpdateDTO {
   status: SubscriptionUpdateType;
-  invoice: InvoiceDto | string;
-  customerBalance: number;
+  subscriptionId: string;
+  currentProductId: string;
+  customerId: string;
+  priceId:string;
+  amount: number;
   errorMessage: string;
+
+  newPlanName: string;
+  newPlanCost: number;
+  billed: 'yearly' | 'monthly';
+
+
+  /**
+   * Indicates the amount of money the user has to pay.
+   * This value should be less than the plan pricing if `moneyBalance` > 0
+   *
+   * @type {number}
+   * @memberof SubscriptionUpdateDTO
+   */
+  toPayNow: number;
+
+  /**
+   * Indicates the amount of money the customer has on our behalf.
+   * if this value != 0, then we won't take $ from the customer
+   * until the balance reach 0
+   *
+   * @type {number}
+   * @memberof SubscriptionUpdateDTO
+   */
+  moneyBalance: number;
+
+  currency: string = 'USD';
 }
 
