4a5bae5ef10264c958db744fe3c7987552190331
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-26 18:09:49 -0500

    Show loading element when requesting to the server

--- src/app/payment/components/subscription-update-confirmation/subscription-update-confirmation.component.ts
+++ src/app/payment/components/subscription-update-confirmation/subscription-update-confirmation.component.ts
@@ -5,6 +5,7 @@ import {
   SubscriptionUpdateType,
 } from '../../dtos/subscription-update.dto';
 import { PaymentService } from '../../services/payment.service';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-subscription-update-confirmation',
@@ -25,7 +26,10 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
     public dialogRef: MatDialogRef<SubscriptionUpdateConfirmationComponent>,
     @Inject(MAT_DIALOG_DATA) public data: SubscriptionUpdateDTO,
     private _paymentService: PaymentService,
-  ) {}
+    private spinner: NgxSpinnerService
+  ) {
+    this.dialogRef.disableClose = true;
+  }
 
   ngOnInit(): void {
     this.isUpgrade = this.data.status === SubscriptionUpdateType.Upgrade;
@@ -35,7 +39,9 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
 
   onConfirm(): void {
     this.isLoading = true;
+    this.spinner.show();
     this._paymentService.updateSubscription(this.data).subscribe((r) => {
+      this.spinner.hide();
       if (r.success) {
         this.dialogRef.close(true);
       }
@@ -48,7 +54,7 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
 
   private setPaymentDetails() {
     this._dataSourcePaymentDetails = [];
-    
+
     if (this.data.moneyBalance < 0) {
       this._dataSourcePaymentDetails.push( {
         label: 'Your Balance',
