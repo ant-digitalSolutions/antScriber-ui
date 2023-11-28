7b3fe82121e01ce0293de99e201a01df546677e9
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-28 18:30:50 -0500

    Improve UX
    - Change text on message
    - Handle error from server

--- src/app/payment/components/subscription-update-confirmation/subscription-update-confirmation.component.ts
+++ src/app/payment/components/subscription-update-confirmation/subscription-update-confirmation.component.ts
@@ -6,6 +6,7 @@ import {
 } from '../../dtos/subscription-update.dto';
 import { PaymentService } from '../../services/payment.service';
 import { NgxSpinnerService } from "ngx-spinner";
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-subscription-update-confirmation',
@@ -26,7 +27,8 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
     public dialogRef: MatDialogRef<SubscriptionUpdateConfirmationComponent>,
     @Inject(MAT_DIALOG_DATA) public data: SubscriptionUpdateDTO,
     private _paymentService: PaymentService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private _snackBar: MatSnackBar
   ) {
     this.dialogRef.disableClose = true;
   }
@@ -44,6 +46,12 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
       this.spinner.hide();
       if (r.success) {
         this.dialogRef.close(true);
+      } else {
+        this.dialogRef.close(false);
+        this._snackBar.open(r.message, undefined, {
+          duration: 4000,
+          panelClass: 'snack-error'
+        });
       }
     });
   }
@@ -57,7 +65,7 @@ export class SubscriptionUpdateConfirmationComponent implements OnInit {
 
     this.dataSourcePaymentDetails.push(
       {
-        label: `Plan ${this.data.newPlanName} (${this.data.currentPlanBilled})`,
+        label: `Plan ${this.data.newPlanName} (${this.data.newPlanBilled})`,
         value: `$${this.data.newPlanCost} ${this.data.currency.toUpperCase()}`,
       },
     )
