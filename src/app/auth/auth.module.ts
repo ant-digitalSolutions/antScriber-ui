022504f3dc87d49b3fcecc0dbee00394c250c845
Adriano Flechilla <adrianof.dev@gmail.com>
2023-11-30 21:24:26 -0500

    Improve UX
    Use a single input element per code char.
    THis way the user has 4 elements to input the code

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import {  MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RegisterWithEmailComponent } from './components/register-with-email/register-with-email.component';
 import { EmailVerificationComponent } from './components/email-verification/email-verification.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 
@@ -35,7 +36,8 @@ import { EmailVerificationComponent } from './components/email-verification/emai
     MatButtonModule,
     FormsModule,
     ReactiveFormsModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    NgxSpinnerModule
   ]
 })
 export class AuthModule { }
