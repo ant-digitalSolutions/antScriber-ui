e96a566245dbc0aed878f59a8df26fe6bf45c523
Adriano Flechilla <adrianof.dev@gmail.com>
2023-12-04 23:06:31 -0500

    Improve UX by showing the password on login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -12,11 +12,9 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-
   options = this.settings.getOptions();
 
   hasInvalidCredentials = false;
@@ -31,6 +29,8 @@ export class LoginComponent implements OnInit {
 
   loginWithPass = false;
 
+  hidePassword = true;
+
   constructor(
     private settings: CoreService,
     private router: Router,
@@ -38,17 +38,21 @@ export class LoginComponent implements OnInit {
     private authService: AuthService,
     protected $gaService: GoogleAnalyticsService,
     private _projectService: BlogProjectsService,
-    private _userService: UserService) { }
+    private _userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.checkIfMobile();
 
-    if (!this.isLoggedIn)
-      this.checkJwtInQueryParams();
+    if (!this.isLoggedIn) this.checkJwtInQueryParams();
   }
 
   form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.minLength(6), Validators.email]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.email,
+    ]),
     password: new FormControl('', [Validators.required]),
   });
 
@@ -67,8 +71,8 @@ export class LoginComponent implements OnInit {
 
     const session = {
       access_token,
-      expiresAt
-    }
+      expiresAt,
+    };
 
     if (showInitialTour) {
       this._userService.showInitialTour = true;
@@ -80,11 +84,14 @@ export class LoginComponent implements OnInit {
 
       if (this.authService.isLoggedIn()) {
         this._projectService.refreshProjects().then(() => {
-          window.history.replaceState(null, '', '/auth/login')
+          window.history.replaceState(null, '', '/auth/login');
           this.router.navigateByUrl('/');
-
-        })
-        this.$gaService.event('user_login', 'user_logged_in', `provider_${provider}`);
+        });
+        this.$gaService.event(
+          'user_login',
+          'user_logged_in',
+          `provider_${provider}`
+        );
       } else {
         this.isLoading = false;
         this.renderContent = true;
@@ -99,54 +106,56 @@ export class LoginComponent implements OnInit {
     this.isLoading = true;
     const userLogin = {
       email: this.form.value.email!,
-      password: this.form.value.password!
+      password: this.form.value.password!,
     };
-    this.authService.login(userLogin).subscribe(
-      (r) => {
-        this.isLoading = false;
-        if (r.success) {
-          this._projectService.refreshProjects().then(() => {
-            this.router.navigateByUrl('/');
-          })
-          this.$gaService.event('user_login', 'user_logged_in', 'provider_password');
-        } else {
-          this.hasInvalidCredentials = true;
-          this.$gaService.event('user_login', 'credential_error', r.error);
-
-        }
+    this.authService.login(userLogin).subscribe((r) => {
+      this.isLoading = false;
+      if (r.success) {
+        this._projectService.refreshProjects().then(() => {
+          this.router.navigateByUrl('/');
+        });
+        this.$gaService.event(
+          'user_login',
+          'user_logged_in',
+          'provider_password'
+        );
+      } else {
+        this.hasInvalidCredentials = true;
+        this.$gaService.event('user_login', 'credential_error', r.error);
       }
-    );
+    });
   }
 
   redirectToGoogleSignIn() {
-    window.location.href = getBaseApiURL() + 'auth/google'
+    window.location.href = getBaseApiURL() + 'auth/google';
   }
 
   redirectToFacebookSignIn() {
-    window.location.href = getBaseApiURL() + 'auth/facebook'
+    window.location.href = getBaseApiURL() + 'auth/facebook';
   }
 
   redirectToLinkedinSignIn() {
-    window.location.href = getBaseApiURL() + 'auth/linkedin'
-    }
+    window.location.href = getBaseApiURL() + 'auth/linkedin';
+  }
 
   checkIfMobile() {
-    this.bigScreen = (window.innerWidth > 1200);
+    this.bigScreen = window.innerWidth > 1200;
   }
 
   continue() {
     this.router.navigate(['/']);
   }
 
+  onHidePassword($event: MouseEvent) {
+    $event.stopPropagation();
+    this.hidePassword = !this.hidePassword;
+  }
 
   public get isLoggedIn(): boolean {
     return this.authService.isLoggedIn();
   }
 
-
   public get userEmail(): string | null {
     return this.authService.userEmail;
   }
-
-
 }
