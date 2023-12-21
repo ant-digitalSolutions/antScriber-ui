9bb1eb1c399f219f86f456ee30961d945a71f91c
Adriano Flechilla <adrianof.dev@gmail.com>
2023-12-20 20:05:35 -0500

    Remove fields from the SMS form

--- src/app/wizard-creator/use-case-meta/data/marketing/use-case-marketing-sms.ts
+++ src/app/wizard-creator/use-case-meta/data/marketing/use-case-marketing-sms.ts
@@ -25,38 +25,38 @@ export class UseCase_Marketing_SMS extends UseCaseMetaAbstract {
         const
             fieldsData: TextFieldToRenderData[] = [];
 
-        fieldsData.push({
-            placeholder: 'E.g. TechStore Ltd.',
-            fieldLabel: 'Business Name',
-            fieldValue: '',
-            validators: [Validators.maxLength(100)],
-            inputMaxLen: 100,
-            dataName: 'business_name',
-            tooltipText: 'Enter the name of your business',
-            isLongText: false
-        });
+        // fieldsData.push({
+        //     placeholder: 'E.g. TechStore Ltd.',
+        //     fieldLabel: 'Business Name',
+        //     fieldValue: '',
+        //     validators: [Validators.maxLength(100)],
+        //     inputMaxLen: 100,
+        //     dataName: 'business_name',
+        //     tooltipText: 'Enter the name of your business',
+        //     isLongText: false
+        // });
 
-        fieldsData.push({
-            placeholder: 'E.g. Jane',
-            fieldLabel: 'Recipient Name',
-            fieldValue: '',
-            validators: [Validators.maxLength(50)],
-            inputMaxLen: 50,
-            dataName: 'recipient_name',
-            tooltipText: 'Enter the name of the recipient',
-            isLongText: false
-        });
+        // fieldsData.push({
+        //     placeholder: 'E.g. Jane',
+        //     fieldLabel: 'Recipient Name',
+        //     fieldValue: '',
+        //     validators: [Validators.maxLength(50)],
+        //     inputMaxLen: 50,
+        //     dataName: 'recipient_name',
+        //     tooltipText: 'Enter the name of the recipient',
+        //     isLongText: false
+        // });
 
-        fieldsData.push({
-            placeholder: 'E.g. Tom from TechStore',
-            fieldLabel: 'Sender Name',
-            fieldValue: '',
-            validators: [Validators.maxLength(50)],
-            inputMaxLen: 50,
-            dataName: 'sender_name',
-            tooltipText: 'Enter your name as the sender',
-            isLongText: false
-        });
+        // fieldsData.push({
+        //     placeholder: 'E.g. Tom from TechStore',
+        //     fieldLabel: 'Sender Name',
+        //     fieldValue: '',
+        //     validators: [Validators.maxLength(50)],
+        //     inputMaxLen: 50,
+        //     dataName: 'sender_name',
+        //     tooltipText: 'Enter your name as the sender',
+        //     isLongText: false
+        // });
 
         fieldsData.push({
             placeholder: 'E.g. SuperFast Charger',
@@ -102,16 +102,16 @@ export class UseCase_Marketing_SMS extends UseCaseMetaAbstract {
             isLongText: false
         });
 
-        fieldsData.push({
-            placeholder: 'E.g. Call Now',
-            fieldLabel: 'Call to Action Text',
-            fieldValue: '',
-            validators: [Validators.maxLength(50)],
-            inputMaxLen: 50,
-            dataName: 'call_to_action_text',
-            tooltipText: 'Enter the text for your call to action button',
-            isLongText: false
-        });
+        // fieldsData.push({
+        //     placeholder: 'E.g. Call Now',
+        //     fieldLabel: 'Call to Action Text',
+        //     fieldValue: '',
+        //     validators: [Validators.maxLength(50)],
+        //     inputMaxLen: 50,
+        //     dataName: 'call_to_action_text',
+        //     tooltipText: 'Enter the text for your call to action button',
+        //     isLongText: false
+        // });
 
         this._wizardFormService.updateFormDefaultField_Text(fieldsData);
     }
